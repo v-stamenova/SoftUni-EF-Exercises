a512ff7df9497746dfe14357c9de3219f61e6442
Valeria Stamenova <valeria.stamenova3@gmail.com>
2021-11-21 11:44:12 +0200

    Add solution to 2. Age Restriction

--- Exercises/BookShop/StartUp.cs
+++ Exercises/BookShop/StartUp.cs
@@ -2,13 +2,30 @@
 {
     using Data;
     using Initializer;
+	using System;
+	using System.Linq;
 
-    public class StartUp
+	public class StartUp
     {
         public static void Main()
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
         }
-    }
+
+		public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+		{
+			var booksWithRestriction = context.Books
+					.Select(b => new
+					{
+						b.Title,
+						b.AgeRestriction
+					})
+					.OrderBy(b => b.Title)
+					.ToList()
+					.Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower());
+
+			return string.Join(Environment.NewLine, booksWithRestriction.Select(x => x.Title).ToList());
+		}
+	}
 }
